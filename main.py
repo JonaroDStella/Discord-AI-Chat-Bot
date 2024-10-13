600b019ac9a981968e3e1e0983bd2bd7b68c90e3
JonaroStella <hs39695422@gmail.com>
2024-10-13 22:57:30 +0800

    fix os.sep difference

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils.Bots import CentralBot
 from config import *
+from os import sep
 
 if __name__ == '__main__':
-    bot = CentralBot(command_prefix=COMMAND_PREFIX, cogs_dir='cogs\\')
+    bot = CentralBot(command_prefix=COMMAND_PREFIX, cogs_dir='cogs'+sep)
     bot.run(TOKEN)
